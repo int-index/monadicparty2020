204935360c9df7cbfcb3d43c6530acd01b0c5a51
Vladislav Zavialov <vlad.z.4096@gmail.com>
2020-02-24 15:12:51 +0300

    More on Peano naturals

--- hs-src/Peano.hs
+++ hs-src/Peano.hs
@@ -1,7 +1,30 @@
 {-# LANGUAGE GADTSyntax #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Peano where
+
+import Numeric.Natural (Natural)
 
 data N where
   Z :: N
   S :: N -> N
+
+deriving instance Show N
+
+fromNatural :: Natural -> N
+fromNatural n =
+  if n == 0
+  then Z
+  else S (fromNatural (n - 1))
+
+toNatural :: N -> Natural
+toNatural Z = 0
+toNatural (S n) = 1 + toNatural n
+
+add :: N -> N -> N
+add Z m = m
+add (S n) m = S (add n m)
+
+mul :: N -> N -> N
+mul Z _ = Z
+mul (S n) m = add m (mul n m)
